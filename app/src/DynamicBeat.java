dc96e28a0a8fd5baa832b50efbabf73265c92051
sungsik-git <kss950708@gamil.com>
2023-12-19 16:08:41 +0900

    refact: UI 라이브러리 JPanel로 변경

--- app/src/DynamicBeat.java
+++ app/src/DynamicBeat.java
@@ -2,51 +2,45 @@ package app.src;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import java.awt.Image;
+import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Image;
 
-
-public class DynamicBeat extends JFrame{
-    
-    private Image ScreenImage;
-    private Graphics screenGraphic;
+public class DynamicBeat extends JFrame {
 
     private Image introBackground;
+    private JPanel img;
 
-    public DynamicBeat(){
+    public DynamicBeat() {
         setTitle("Dynamic Beat");
-
         setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-        //Do not resizing by user
         setResizable(false);
-        //Exactly center position when running
         setLocationRelativeTo(null);
-        //Close both Jfram and program
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
-        //Get background image
+        // Get background image
         ImageIcon icon = new ImageIcon(Main.class.getResource("../images/background(title).jpg"));
         Image originalImage = icon.getImage();
-        //Resizing to original image
+        // Resizing to original image
         introBackground = originalImage.getScaledInstance(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, Image.SCALE_SMOOTH);
 
-        //IntroBGM paly
+        // IntroBGM play
         Music introMusic = new Music("introBGM.mp3", true);
         introMusic.start();
-    }
-    
 
-    //JFrame Screen Layout
-    public void paint(Graphics g){
-        ScreenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-        screenGraphic = ScreenImage.getGraphics();
-        screenDraw(screenGraphic);
-        g.drawImage(ScreenImage, 0, 0, null);
-    }
+        
+        img = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(introBackground, 0, 0, this);
+            }
+        };
+        img.setLayout(null);
+        img.setBounds(0, 0, Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        add(img);
 
-    public void screenDraw(Graphics g){
-        g.drawImage(introBackground, 0, 0, null);
-        this.repaint();
+        setVisible(true);
     }
 }
+
